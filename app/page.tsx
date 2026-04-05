d8e9c5f21b5ff4f9deec199775ea7c1807e02f2f
CHAITSNIPER <email>
2026-04-05 16:16:33 +0530

    heoo

--- app/page.tsx
+++ app/page.tsx
@@ -79,7 +79,7 @@ useEffect(() => {
           onClick={() => setCurrentStep('data')}
           className={`px-4 py-2 text-sm font-semibold hover:cursor-pointer rounded ${currentStep === 'data' ? 'bg-white text-green-800' : 'bg-green-700 text-white hover:bg-green-600'}`}
         >
-          Your data with us
+          Your data
         </button>
 
         
